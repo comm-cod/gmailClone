e14af97e97823cfa676f344422d11c0c0d589f8f
comm-cod <128215889+comm-cod@users.noreply.github.com>
2024-05-26 07:36:01 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'compose': '#c2e7ff',
+        'inbox': '#d3e3fd',
+        'primary': '#0000ff'
+      }
+    },
+  },
+  plugins: [],
+}
+
